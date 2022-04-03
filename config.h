9560bb8725a5cdaadf8b2056d5705b34faf47f9b
SaintB2 <busterc246@gmail.com>
2022-04-03 15:33:31 +0000

    Changed font to Jetbrains Mono.

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "open sans:size=10" };
-static const char dmenufont[]       = "open sans:size=10";
+static const char *fonts[]          = { "Jetbrains Mono:size=10" };
+static const char dmenufont[]       = "Jetbrains Mono:size=10";
 // Background color
 static const char col_gray1[]       = "#1d2021";
 // Inactive background color
